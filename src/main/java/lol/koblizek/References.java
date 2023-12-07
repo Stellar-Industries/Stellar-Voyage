42cb5052d858399c1bb5881ae60350133397d05a
KoblizekXD <janprokupek@centrum.cz>
2023-12-07 16:01:00 +0100

    feat: move logger

--- src/main/java/lol/koblizek/References.java
+++ src/main/java/lol/koblizek/References.java
@@ -1,7 +1,11 @@
 package lol.koblizek;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class References {
     private References() {}
 
     public static final String MOD_ID = "stellarvoyage";
+    public static final Logger LOGGER = LoggerFactory.getLogger(References.MOD_ID);
 }
--- src/main/java/lol/koblizek/StellarVoyageMod.java
+++ src/main/java/lol/koblizek/StellarVoyageMod.java
@@ -7,8 +7,6 @@ import org.slf4j.LoggerFactory;
 
 public class StellarVoyageMod implements ModInitializer {
 
-    public static final Logger LOGGER = LoggerFactory.getLogger(References.MOD_ID);
-
 	@Override
 	public void onInitialize() {
 
