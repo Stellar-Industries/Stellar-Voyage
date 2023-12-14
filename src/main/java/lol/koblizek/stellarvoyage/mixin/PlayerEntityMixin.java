ad414b539efbe98e9dd7ac4c0294caa3bc0f03da
KoblizekXD <janprokupek@centrum.cz>
2023-12-14 19:05:16 +0100

    add particle to coal burning + support for more ores

--- src/main/java/lol/koblizek/stellarvoyage/mixin/PlayerEntityMixin.java
+++ src/main/java/lol/koblizek/stellarvoyage/mixin/PlayerEntityMixin.java
@@ -2,24 +2,30 @@ package lol.koblizek.stellarvoyage.mixin;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.entity.boss.BossBar;
-import net.minecraft.entity.damage.DamageEffects;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.damage.DamageType;
-import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.particle.DustParticleEffect;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.text.Text;
-import net.minecraft.text.TextColor;
+import net.minecraft.util.Colors;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.Box;
+import org.joml.Vector3f;
+import org.lwjgl.system.MathUtil;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.awt.*;
+import java.util.Arrays;
+
 @Mixin(PlayerEntity.class)
-public class PlayerEntityMixin {
+public abstract class PlayerEntityMixin {
+
+	private static final Block[] BURNING_MATERIALS = {
+			Blocks.COAL_ORE
+	};
 
 	@Inject(at = @At("HEAD"), method = "tick")
 	public void tick(CallbackInfo ci) {
@@ -27,9 +33,12 @@ public class PlayerEntityMixin {
 		if (!player.isCreative()) {
 			player.getWorld().getStatesInBox(new Box(player.getBlockPos().add(-2, -2, -2).toCenterPos(), player.getBlockPos().add(2, 2, 2).toCenterPos())).forEach((blockState) -> {
 				Block block = blockState.getBlock();
-				if (block == Blocks.COAL_ORE) {
-					player.sendMessage(Text.literal(Formatting.RED + "My lungs are burning!"), true);
-					player.damage(player.getWorld().getDamageSources().generic(), 1);
+				for (Block material : BURNING_MATERIALS) {
+					if (material.equals(block)) {
+						player.getWorld().addParticle(new DustParticleEffect(new Vector3f(0f, 0f, 0f), 1.5f), player.getX(), player.getY() + 1.6, player.getZ(), 0.0D, 0.0D, 0.0D);
+						player.sendMessage(Text.of(Formatting.RED + "My lungs are burning!"), true);
+						player.damage(player.getWorld().getDamageSources().generic(), 1);
+					}
 				}
 			});
 		}
