822262aa229479179b2ba8b9275362ed18d37012
KoblizekXD <janprokupek@centrum.cz>
2023-12-14 19:36:27 +0100

    array randoms

--- src/main/java/lol/koblizek/stellarvoyage/util/Utils.java
+++ src/main/java/lol/koblizek/stellarvoyage/util/Utils.java
@@ -3,9 +3,18 @@ package lol.koblizek.stellarvoyage.util;
 import org.joml.Vector3f;
 
 import java.awt.*;
+import java.util.Random;
 
 public class Utils {
     public static Vector3f colorToVec(Color color) {
         return new Vector3f(color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f);
+    }
+
+    public static int random(int min, int max) {
+        return new Random().nextInt(min , max - 1);
+    }
+
+    public static <T> T random(T[] array) {
+        return array[random(0, array.length-1)];
     }
 }
