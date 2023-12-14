f185809586c9fa0a620dc0feb7a7b5304621f215
KoblizekXD <janprokupek@centrum.cz>
2023-12-14 14:26:54 +0100

    remove crafting recipes

--- src/main/java/lol/koblizek/stellarvoyage/mixin/PlayerEntityMixin.java
+++ src/main/java/lol/koblizek/stellarvoyage/mixin/PlayerEntityMixin.java
@@ -1,15 +1,17 @@
 package lol.koblizek.stellarvoyage.mixin;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin {
+
+	@Inject(at = @At("HEAD"), method = "tick")
+	public void tick(CallbackInfo ci) {
+		
 	}
 }
