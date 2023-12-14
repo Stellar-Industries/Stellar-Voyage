f38537a0bb1657e07e97ef7190e2252115abbe07
KoblizekXD <janprokupek@centrum.cz>
2023-12-14 20:03:08 +0100

    change join message 2

--- src/client/java/lol/koblizek/stellarvoyage/StellarVoyageClient.java
+++ src/client/java/lol/koblizek/stellarvoyage/StellarVoyageClient.java
@@ -7,10 +7,14 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+
+import java.text.Normalizer;
 
 public class StellarVoyageClient implements ClientModInitializer, References {
 	@Override
@@ -19,6 +23,8 @@ public class StellarVoyageClient implements ClientModInitializer, References {
 	}
 
 	public void onPlayReady(ClientPlayNetworkHandler handler, PacketSender sender, MinecraftClient client) {
-		client.player.sendMessage(Text.literal("Loaded world with Stellar Voyage!").formatted(Formatting.YELLOW));
+		client.player.sendMessage(Text.literal("K").formatted(Formatting.DARK_PURPLE, Formatting.OBFUSCATED)
+				.append(Text.literal("Welcome long lost adventurer, the world welcomes you").formatted(Formatting.GOLD))
+				.append(Text.literal("K").formatted(Formatting.OBFUSCATED, Formatting.DARK_PURPLE)));
 	}
 }
