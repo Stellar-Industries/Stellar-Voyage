2fcd3b9e6bd193beb6f2c8af14709a98950c1f25
KoblizekXD <janprokupek@centrum.cz>
2023-12-15 17:34:41 +0100

    change mixin to callback for tooltips

--- src/client/java/lol/koblizek/stellarvoyage/StellarVoyageClient.java
+++ src/client/java/lol/koblizek/stellarvoyage/StellarVoyageClient.java
@@ -1,9 +1,11 @@
 package lol.koblizek.stellarvoyage;
 
+import lol.koblizek.stellarvoyage.item.ItemCallbacks;
 import lol.koblizek.stellarvoyage.util.References;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.client.MinecraftClient;
@@ -15,11 +17,20 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
 import java.text.Normalizer;
+import java.util.Random;
 
 public class StellarVoyageClient implements ClientModInitializer, References {
 	@Override
 	public void onInitializeClient() {
+		ItemCallbacks.register();
+		ClientLifecycleEvents.CLIENT_STARTED.register(this::onClientStarted);
 		ClientPlayConnectionEvents.JOIN.register(this::onPlayReady);
+	}
+
+	public void onClientStarted(MinecraftClient client) {
+		if (new Random().nextBoolean()) {
+			MinecraftClient.getInstance().getWindow().setTitle("Tisice lidi sukaji denne v teto hre...");
+		}
 	}
 
 	public void onPlayReady(ClientPlayNetworkHandler handler, PacketSender sender, MinecraftClient client) {
